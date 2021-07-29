a0704bef4ba21e19bcd5f49b84db7f4ba4d70faf
vntralu <rafael.albano@venturus.org.br>
2021-07-29 11:23:43 -0300

    Saving Changes challenge completed

--- src/index.js
+++ src/index.js
@@ -10,19 +10,73 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404). json({ error: "Username not found !"});
+  }
+
+  request.user = user;
+
+  next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const withinTheLimitFrePan = (user.pro === false && user.todos.length < 10);
+
+  if(!user.pro && !withinTheLimitFrePan){
+    return response.status(403).json({ error: "Access Denied"});
+  }
+  
+  next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+
+  const { username } = request.headers;
+  const { id } = request.params;
+  // const { user } = request;
+
+  const is_uuid = validate(id);
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404). json({ error: "User not Found!"});
+  }
+
+  if(!is_uuid){
+    return response.status(400).json({ error: "ID invvalid " });
+  }
+
+  const todo = user.todos.find(todo => todo.id === id);
+  
+  if(!todo){
+    return response.status(404).json({ error: "Todo not Found" });
+  }
+
+  request.todo = todo;
+  request.user = user;
+
+  next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404). json({ error: "User not found !"});
+  }
+
+  request.user = user;
+
+  next();
 }
 
 app.post('/users', (request, response) => {
